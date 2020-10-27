c86d21bd0288fc103adbc087121dc00f0690c3a5
niluhwukushintadewi <Dewishinta2120@gmail.com>
2020-10-26 23:00:45 -0700

    praktikum bootstrap 27/10

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -5,6 +5,15 @@
  */
 class mahasiswa extends CI_Controller
 {
+	public function index($url_nim ='',$url_jur ='')
+	{
+       $data['nama'] = 'ni luh wuku shinta dewi';
+       $data['umur'] = '20';
+       $data['nim']  = $url_nim;
+       $data['jurusan'] = $url_jur;
+
+		$this->load->view('mahasiswa/v_index',$data);
+	}
 	
 	public function jurusan()
 	{
---
+++ application/views/mahasiswa/v_index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Data mahasiswa
+	</title>
+	<link rel="stylesheet" 
+	      href="<?= base_url('assets/bootstrap/css/bootstrap.min.css');?>">
+</head>
+<body>
+	<h2>Data mahasiswa</h2>
+	<p>Nama : <?php echo $nama;?></p>
+	<p>umur : <?php echo $umur;?></p>
+	<p>nim  : <?php echo $nim;?></p>
+	<P>jurusan: <?php echo $jurusan?></P>
+</body>
+</html>
